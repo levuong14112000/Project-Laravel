ee0e831c7ed0ca8b9c1199af91b68e01bf1b8842
levuong14112000 <levuong2000gl@gmail.com>
2023-02-24 20:04:49 +0700

    'ok'

---
+++ app/Http/Controllers/myController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class myController extends Controller
+{
+    public function main(){
+        return view('main');
+    }
+}
