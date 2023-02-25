518b126ef58547356cb4b917e7accce11204e086
levuong14112000 <125888167+levuong14112000@users.noreply.github.com>
2023-02-25 23:41:35 +0700

    Update myController.php

--- app/Http/Controllers/myController.php
+++ app/Http/Controllers/myController.php
@@ -1,13 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class myController extends Controller
 {
     public function main(){
-        return view('main');
+        return view('layout.main');
+    }
+    public function khoahoc(){
+        $query =DB::table('courses') //Sử dụng class DB
+        ->select("course_id","course_name","description","price","picture")
+            ->orderBy('course_name','ASC')
+            ->get();
+        return view('khoahoc')->with('ds',$query);
     }
     public function detail1(){
         return view('detailkhoahoc1');
@@ -18,4 +25,5 @@ class myController extends Controller
     public function detail3(){
         return view('detailkhoahoc3');
     }
+
 }
