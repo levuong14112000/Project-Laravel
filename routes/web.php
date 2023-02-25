c9765453cf7c8d4986fb1664795e35590357c0a0
levuong14112000 <levuong2000gl@gmail.com>
2023-02-25 22:48:19 +0700

    fist commit

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/', function () {
 });
 Route::get('/main',[\App\Http\Controllers\myController::class,'main']);
 Route::get('/khoahoc1',[\App\Http\Controllers\myController::class,'detail1']);
-Route::get('/khoahoc2',[\App\Http\Controllers\myController::class,'detail']);
+Route::get('/khoahoc2',[\App\Http\Controllers\myController::class,'detail2']);
 Route::get('/khoahoc3',[\App\Http\Controllers\myController::class,'detail3']);
