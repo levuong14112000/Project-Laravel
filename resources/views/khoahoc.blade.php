4d686b3c11176d914fcc2689a14086a26c58df98
levuong14112000 <125888167+levuong14112000@users.noreply.github.com>
2023-02-25 23:40:51 +0700

    Update khoahoc.blade.php

--- resources/views/khoahoc.blade.php
+++ resources/views/khoahoc.blade.php
@@ -1,21 +1,43 @@
 @extends('layout.main')
 @section('khoahoc1')
-    Hello 1
-@endsection
+    @foreach($ds as $n)
+        @if($n->course_id == 1)
+            <div class="d-flex flex-column align-items-center col-3 form-group" style="float: right">
+                <div class="hand"><img class="img-thumbnail" src="{{asset('uploads/images/'.$n->picture)}}" alt=""></div>
+                <div class="text-center">{{$n->course_name}}</div>
+                <div class="pb-1 btn btn-button ">{{number_format($n->price)}} VNĐ</div>
+                <div>
+                    <a class="btn btn-primary" href="http://127.0.0.1:8000/khoahoc1">Detail</a>
+                </div>
+            </div>  d
+        @endif
+    @endforeach
+            @endsection
 @section('khoahoc2')
-    Hello 2
-@endsection
-@section('khoahoc3')
-    Hello 3
-@endsection
-@foreach($ds1 as $n)
-    @if($n->makhoa == 1)
-        <div class="d-flex flex-column align-items-center col-3 form-group" style="float: right">
-            <div class="hand"><img class="img-thumbnail" src="{{asset('uploads/images/'.$n->hinh)}}" alt=""></div>
-            <div class="text-center">{{$n->tensp}}</div>
-            <div class="pb-1 btn btn-button ">{{number_format($n->donggia)}} VNĐ</div>
-            <div>
-                <a class="btn btn-primary" href="http://127.0.0.1:8000/khoahoc1">Detail</a>
+    @foreach($ds as $n)
+        @if($n->course_id == 2)
+            <div class="d-flex flex-column align-items-center col-3 form-group" style="float: right">
+                <div class="hand"><img class="img-thumbnail" src="{{asset('uploads/images/'.$n->picture)}}" alt=""></div>
+                <div class="text-center">{{$n->course_name}}</div>
+                <div class="pb-1 btn btn-button ">{{number_format($n->price)}} VNĐ</div>
+                <div>
+                    <a class="btn btn-primary" href="http://127.0.0.1:8000/khoahoc1">Detail</a>
+                </div>
             </div>
-            <div>hello</div>
-        </div>
+        @endif
+    @endforeach
+            @endsection
+@section('khoahoc3')
+    @foreach($ds as $n)
+        @if($n->course_id == 3)
+            <div class="d-flex flex-column align-items-center col-3 form-group" style="float: right">
+                <div class="hand"><img class="img-thumbnail" src="{{asset('uploads/images/'.$n->picture)}}" alt=""></div>
+                <div class="text-center">{{$n->course_name}}</div>
+                <div class="pb-1 btn btn-button ">{{number_format($n->price)}} VNĐ</div>
+                <div>
+                    <a class="btn btn-primary" href="http://127.0.0.1:8000/khoahoc1">Detail</a>
+                </div>
+            </div>
+        @endif
+    @endforeach
+            @endsection
